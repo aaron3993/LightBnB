b3d28d8cfd66b61a153fe77e7414f97c59442e8a
Aaron <aaron3993@hotmail.com>
2020-06-25 23:14:19 +0000

    Put all the functions into one module.export and changed them into object methods

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,6 +1,6 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
-const db = require('./db')
+const db = require('./db');
 /// Users
 
 /**
@@ -8,60 +8,58 @@ const db = require('./db')
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
-  return db.query(`
+module.exports = {
+  getUserWithEmail: (email) => {
+    return db.query(`
     SELECT * FROM users
     WHERE email = $1`, [email])
-    .then(res => {
-      if (res.rows.length) {
-        return res.rows[0]
-      } else {
-        return null
-      }
-    })
-    .catch(err => console.error('query error', err.stack));
-}
-exports.getUserWithEmail = getUserWithEmail;
+      .then(res => {
+        if (res.rows.length) {
+          return res.rows[0];
+        } else {
+          return null;
+        }
+      })
+      .catch(err => console.error('query error', err.stack));
+  },
 
-/**
+  /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
-  return db.query(`
+  getUserWithId: (id) => {
+    return db.query(`
   SELECT * FROM users
   WHERE id = $1`, [id])
-  .then(res => res.rows[0])
-  .catch(err => console.error('query error', err.stack));
+      .then(res => res.rows[0])
+      .catch(err => console.error('query error', err.stack));
   // return Promise.resolve(users[id]);
-}
-exports.getUserWithId = getUserWithId;
+  },
 
 
-/**
+  /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  return db.query(`
+  addUser: (user) => {
+    return db.query(`
   INSERT INTO users (name, email, password)
   VALUES ($1, $2, $3)
   RETURNING *;`, [user.name, user.email, user.password])
-  .then(res => res.rows)
-  .catch(err => console.error('query error', err.stack));
-}
-exports.addUser = addUser;
-/// Reservations
+      .then(res => res.rows)
+      .catch(err => console.error('query error', err.stack));
+  },
+  /// Reservations
 
-/**
+  /**
  * Get all reservations for a single user.
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return db.query(`
+  getAllReservations: (guest_id, limit = 10) => {
+    return db.query(`
   SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
   FROM reservations
   JOIN properties ON properties.id = reservations.property_id
@@ -70,82 +68,80 @@ const getAllReservations = function(guest_id, limit = 10) {
   GROUP BY reservations.id, properties.id
   ORDER BY start_date
   LIMIT $2;`, [guest_id, limit])
-  .then(res => res.rows)
-  .catch(err => console.error('query error', err.stack));
-}
-exports.getAllReservations = getAllReservations;
+      .then(res => res.rows)
+      .catch(err => console.error('query error', err.stack));
+  },
 
-/// Properties
+  /// Properties
 
-/**
+  /**
  * Get all properties.
  * @param {{}} options An object containing query options.
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-  const getAllProperties = function(options, limit = 10) {
-      // 1
-  const queryParams = [];
-  // 2
-  let queryString = `
+  getAllProperties: (options, limit = 10) => {
+    // 1
+    const queryParams = [];
+    // 2
+    let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id
   `;
-  // 3
-  if (options.city) {
-    queryParams.push(`%${options.city}%`)
-    queryString += `WHERE city LIKE $${queryParams.length}`
-  }
-  if (options.owner_id) {
-    queryParams.push(`${options.owner_id}`);
-    queryString += `${queryParams.length === 1 ? 'WHERE' : ' AND'} owner_id LIKE $${queryParams.length}`;
-  }
-  if (options.minimum_price_per_night && options.maximum_price_per_night) {
-    queryParams.push(`${options.minimum_price_per_night * 100}`);
-    const minParam = queryParams.length
-    queryParams.push(`${options.maximum_price_per_night * 100}`);
-    const maxParam = queryParams.length
-    queryString += `${queryParams.length === 2 ? 'WHERE' : ' AND'} cost_per_night BETWEEN $${minParam} AND $${maxParam}`;
-  } else if (options.minimum_price_per_night) {
-    queryParams.push(`${options.minimum_price_per_night * 100}`);
-    queryString += `${queryParams.length === 1 ? 'WHERE' : ' AND'} cost_per_night > $${queryParams.length}`;
-  } else {
-    queryParams.push(`${options.maximum_price_per_night * 100}`);
-    queryString += `${queryParams.length === 1 ? 'WHERE' : ' AND'} cost_per_night < $${queryParams.length}`;
-  }
-  if (options.minimum_rating) {
-    queryParams.push(`${options.minimum_rating}`);
-    queryString += `${queryParams.length === 1 ? 'WHERE' : ' AND'} rating > $${queryParams.length}`;
-  }
+    // 3
+    if (options.city) {
+      queryParams.push(`%${options.city}%`);
+      queryString += `WHERE city LIKE $${queryParams.length}`;
+    }
+    if (options.owner_id) {
+      queryParams.push(`${options.owner_id}`);
+      queryString += `${queryParams.length === 1 ? 'WHERE' : ' AND'} owner_id LIKE $${queryParams.length}`;
+    }
+    if (options.minimum_price_per_night && options.maximum_price_per_night) {
+      queryParams.push(`${options.minimum_price_per_night * 100}`);
+      const minParam = queryParams.length;
+      queryParams.push(`${options.maximum_price_per_night * 100}`);
+      const maxParam = queryParams.length;
+      queryString += `${queryParams.length === 2 ? 'WHERE' : ' AND'} cost_per_night BETWEEN $${minParam} AND $${maxParam}`;
+    } else if (options.minimum_price_per_night) {
+      queryParams.push(`${options.minimum_price_per_night * 100}`);
+      queryString += `${queryParams.length === 1 ? 'WHERE' : ' AND'} cost_per_night > $${queryParams.length}`;
+    } else {
+      queryParams.push(`${options.maximum_price_per_night * 100}`);
+      queryString += `${queryParams.length === 1 ? 'WHERE' : ' AND'} cost_per_night < $${queryParams.length}`;
+    }
+    if (options.minimum_rating) {
+      queryParams.push(`${options.minimum_rating}`);
+      queryString += `${queryParams.length === 1 ? 'WHERE' : ' AND'} rating > $${queryParams.length}`;
+    }
 
-  // 4
-  queryParams.push(limit);
-  queryString += `
+    // 4
+    queryParams.push(limit);
+    queryString += `
   GROUP BY properties.id
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
 
-  // 5
-  console.log(queryString, queryParams);
+    // 5
+    console.log(queryString, queryParams);
 
-  // 6
-  return db.query(queryString, queryParams)
-  .then(res => res.rows)
-  .catch(err => console.error('query error', err.stack));
-}
-exports.getAllProperties = getAllProperties;
+    // 6
+    return db.query(queryString, queryParams)
+      .then(res => res.rows)
+      .catch(err => console.error('query error', err.stack));
+  },
 
 
-/**
+  /**
  * Add a property to the database
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
-  console.log(property)
-  return db.query(`
+  addProperty: (property) => {
+    console.log(property);
+    return db.query(`
     INSERT INTO properties (
       title,
       description,
@@ -164,7 +160,7 @@ const addProperty = function(property) {
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
     RETURNING *;
     `, [property.title, property.description, property.number_of_bedrooms, property.number_of_bathrooms, property.parking_spaces, property.cost_per_night, property.thumbnail_photo_url, property.cover_photo_url, property.street, property.country, property.city, property.province, property.post_code, property.own_id])
-    .then(res => res.rows)
-    .catch(err => console.error('query error', err.stack));
-}
-exports.addProperty = addProperty;
+      .then(res => res.rows)
+      .catch(err => console.error('query error', err.stack));
+  }
+};
