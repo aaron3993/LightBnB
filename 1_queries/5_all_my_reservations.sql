9713c386c767aec9701095286916bd850bb62c54
Aaron <aaron3993@hotmail.com>
2020-06-25 17:18:53 +0000

    Added queries for getUserWithEmail, getUserWithId, AddUser, and getAllReservations

--- 1_queries/5_all_my_reservations.sql
+++ 1_queries/5_all_my_reservations.sql
@@ -1,28 +1,3 @@
-All My Reservations
-When a user is logged in, they will have an option to view all of their reservations. This page will show details about a reservation and details about the property associated with the reservation.
-
-Show all reservations for a user.
-
-Select all columns from the reservations table, all columns from the properties table, and the average rating of the property.
-The reservations will be for a single user, so use 1 for the user_id.
-Order the results from the earliest start_date to the most recent start_date.
-These will end up being filtered by either "Upcoming Reservations" or "Past Reservations", so only get reservations where the end_date is in the past.
-Use now()::date to get today's date.
-Limit the results to 10.
-Expected Result:
-
-We're only showing the id, title, start_date, and cost_per_night to save space on the screen. As time progresses, so will the returned timestamp from the now function. As such, your output may return more rows than what is shown in the example.
-
- id  |      title      | cost_per_night | start_date |   average_rating   
------+-----------------+----------------+------------+--------------------
- 931 | Apple barn      |          79787 | 2014-05-17 | 4.1666666666666667
- 209 | Piano principle |          16669 | 2014-08-17 | 3.7777777777777778
- 994 | Bow forest      |          13562 | 2015-07-30 | 4.2727272727272727
- 218 | Down observe    |          36117 | 2016-05-11 | 4.2857142857142857
- 129 | Like arrow      |          92451 | 2016-07-08 | 4.1666666666666667
- 276 | You weight      |          11459 | 2017-07-01 | 3.8333333333333333
-(6 rows)
-
 SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
 FROM reservations
 JOIN properties ON properties.id = reservations.property_id
--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,13 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
 
 /// Users
 
@@ -9,16 +17,28 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool.query(`
+    SELECT * FROM users
+    WHERE email = $1`, [email])
+    .then(res => {
+      if (res.rows.length) {
+        return res.rows[0]
+      } else {
+        console.log('null')
+        return null
+      }
+    })
+    .catch(err => console.error('query error', err.stack));
+  // let user;
+  // for (const userId in users) {
+  //   user = users[userId];
+  //   if (user.email.toLowerCase() === email.toLowerCase()) {
+  //     break;
+  //   } else {
+  //     user = null;
+  //   }
+  // }
+  // return Promise.resolve(user);
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -28,7 +48,12 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`
+  SELECT * FROM users
+  WHERE id = $1`, [id])
+  .then(res => res.rows[0])
+  .catch(err => console.error('query error', err.stack));
+  // return Promise.resolve(users[id]);
 }
 exports.getUserWithId = getUserWithId;
 
@@ -39,13 +64,18 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool.query(`
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;`, [user.name, user.email, user.password])
+  .then(res => res.rows)
+  .catch(err => console.error('query error', err.stack));
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
 }
 exports.addUser = addUser;
-
 /// Reservations
 
 /**
@@ -54,7 +84,21 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+  FROM reservations
+  JOIN properties ON properties.id = reservations.property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE property_reviews.guest_id = $1 and end_date < now()::date
+  GROUP BY reservations.id, properties.id
+  ORDER BY start_date
+  LIMIT $2;`, [guest_id, limit])
+  .then(res => {
+    console.log(res.rows)
+    return res.rows})
+  .catch(err => console.error('query error', err.stack));
+
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
@@ -66,12 +110,20 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  const getAllProperties = function(options, limit = 10) {
+  // options = `SELECT *
+  // FROM properties;`
+  // const limitedProperties = {};
+  // for (let i = 1; i <= limit; i++) {
+  //   limitedProperties[i] = properties[i];
+  // }
+  // return Promise.resolve(limitedProperties);
+  return pool.query(`
+  SELECT * FROM properties
+  LIMIT $1
+  `, [limit])
+  .then(res => res.rows)
+  .catch(err => console.error('query error', err.stack));
 }
 exports.getAllProperties = getAllProperties;
 
